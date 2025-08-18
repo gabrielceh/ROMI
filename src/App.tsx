9e86305d8aa995861317761a9895a0717ed8fe81
gabrielceh <gabrielcehu@gmail.com>
2025-08-18 09:57:53 -0500

    añadiendo dependencias y primeras configuraciones

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button';
+
+function App() {
+	return (
+		<>
+			<div className='text-4xl text-red-400'>ROMI</div>
+			<br />
+			<Button>Botón</Button>
+		</>
+	);
+}
+
+export default App;
